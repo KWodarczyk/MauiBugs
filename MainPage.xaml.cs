dcc5612378ca4dccc55c06844c51824ac98c0747
Karol <ironhide8391@googlemail.com>
2023-03-03 21:46:03 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
