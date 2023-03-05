f5ef4f5243b14f943f4591b72b2244a68f635dcc
IronHide <ironhide8391@googlemail.com>
2023-03-05 23:34:15 +0100

    wip

---
+++ MauiTest/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var popup = new PopupView();
+
+            this.ShowPopup(popup);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new TestPage());
+        }
+    }
+}
---
+++ MauiTest/TestView.xaml.cs
@@ -0,0 +1,30 @@
+namespace MauiTest;
+
+public partial class TestView : ContentView
+{
+
+	public TestView()
+	{
+
+        InitializeComponent();
+
+        var list1 = new List<int>();
+
+        for (int i = 0; i < 60; i++)
+        {
+            list1.Add(i);
+        }
+
+        List1.ItemsSource = list1;
+
+        var list2 = new List<int>();
+
+        for (int i = 0; i < 60; i++)
+        {
+            list2.Add(i);
+        }
+
+        List2.ItemsSource = list2;
+
+    }
+}
