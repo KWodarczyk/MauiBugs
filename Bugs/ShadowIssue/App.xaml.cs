b8174a7bb79bfa61dc7f6ab052a07d4c79f7e2f5
IronHide <ironhide8391@googlemail.com>
2023-03-05 23:44:08 +0100

    wip

---
+++ Bugs/ShadowIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShadowIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
